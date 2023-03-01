bda009b073a22c8a0fb72d87f09aa3347449efc2
Niroj Thapa <ngtc0925@gmail.com>
2023-03-01 16:23:09 +0545

    settime out function useeffect

--- pages/404.js
+++ pages/404.js
@@ -1,17 +1,27 @@
 //this is our own custome page is any page is not found 
 import Link from 'next/link'
 import {useRouter} from 'next/router';
+import { useEffect } from 'react';
 
 const noPage=()=>{
     const router=useRouter();
+  const handleClick=()=>{
+    router.push('/');
+  }
 
+  useEffect(()=>{
+    //this will redirect homepage after 5 seconds when typed incorrect url
+    setTimeout(()=>{
+        router.push('/')
+    },5000)
+  })
     return(
         <>
         <h3>404</h3>
         <h3>page not found gaich</h3>
         <Link href='/'>Home</Link>
         <br/>
-        <a onClick={()=>router.push('/')} style={{cursor:'pointer'}}>Back to Home</a>
+        <a onClick={handleClick} style={{cursor:'pointer'}}>Back to Home</a>
         </>
     )
 }
