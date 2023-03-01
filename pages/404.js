17bc54893d3971c79f80fa61d43581bd972d5892
Niroj Thapa <ngtc0925@gmail.com>
2023-03-01 16:10:21 +0545

    onclick

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,17 @@
 //this is our own custome page is any page is not found 
 import Link from 'next/link'
+import {useRouter} from 'next/router';
+
 const noPage=()=>{
+    const router=useRouter();
+
     return(
         <>
         <h3>404</h3>
         <h3>page not found gaich</h3>
         <Link href='/'>Home</Link>
+        <br/>
+        <a onClick={()=>router.push('/')} style={{cursor:'pointer'}}>Back to Home</a>
         </>
     )
 }
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,10 +1,10 @@
-
+//to access blog folder with their respective name which is of name index.js file name 
 import styles from '@/styles/Home.module.css'
 
 const blog=()=> {
   return (
     <>
-        <p>Hello this is a blog page</p>
+        <p>Hello this is a blog page which is in blog folder</p>
 
     </>
   )
