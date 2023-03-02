692b413b44951eeb73e9013946542d997df5a974
Niroj Thapa <ngtc0925@gmail.com>
2023-03-02 07:41:02 +0545

    api call getStaticProps

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,13 +1,34 @@
 //to access blog folder with their respective name which is of name index.js file name 
 import Navbar from '@/component/Navbar';
-import style from '@/styles/blog.module.css';
+import styles from '@/styles/blog.module.css';
 
+export const getStaticProps=async()=>{
+  const res=await fetch('https://jsonplaceholder.typicode.com/posts')
+  //parse into json form which in array
+  const posts=await res.json();
+  return{
+    props:{
+      posts,
+    }
+  }
+  
+}
 
-const blog=()=> {
+const blog=({posts})=> {
   return (
     <>
     <Navbar/>
-        <p className={style.text}>Hello this is a blog page which is in blog folder</p> 
+    <p className={styles.text}>Hello this is a blog page which is in blog folder</p> 
+
+        {( posts.slice(0,5).map((item)=>{
+            return (
+              <div key={item.id} className={styles.ssr_styles}>
+                <h2>{item.id}</h2>
+                <h3>{item.title}</h3>
+              </div>
+            )
+          }))
+        }
     </>
   )
 }
